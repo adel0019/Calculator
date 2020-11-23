c8bf9bba146a962312fec519f775767426810d4a
Mohamed Adel <adel0019@algonquinlive.com>
2020-11-22 21:38:48 -0500

    saving

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ function printOutput(num) {
     }
 }
 function getFormattedNumber(num) {
+    if(num=="=") {
+        return "";
+    }
     var n =Number(num);
     var value = n.toLocaleString("en");
     return value;
@@ -27,7 +30,41 @@ function reverseNumberFormat(num) {
 var operator = document.getElementsByClassName("operator");
 for (var i=0;i<operator.length;i++) {
     operator[i].addEventListener("click", function() {
-        
+        if(this.id=="clear") {
+            printHistory("");
+            printOutput("");
+        }
+        else if(this.id=="backspace") {
+            var output = reverseNumberFormat(getOutput()).toString();
+            if(output){
+                output = output.substr(0,output.length-1);
+                printOutput(output);
+            }
+        }
+        else {
+            var output=getOutput();
+            var history=getHistory();
+            if(output==""&&history!="") {
+                if(isNaN(history[history.length-1])) {
+                    history=history.substr(0,history.length-1)
+                }
+            }
+            if(output !="" || history!=""){
+                output=output==""?
+                output:reverseNumberFormat(output);
+                history=history+output;
+                if(this.id=="=") {
+                    var result=eval(history);
+                    printOutput(result);
+                    printHistory("");
+                }
+                else {
+                    history=history+this.id;
+                    printHistory(history);
+                    printOutput("");
+                }
+            }
+        }
     });
 }
 var number = document.getElementsByClassName("number");
