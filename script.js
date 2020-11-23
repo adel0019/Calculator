0723ca5640228e0808bc0e8f9c317b040fa6abad
Mohamed Adel <adel0019@algonquinlive.com>
2020-11-22 21:10:15 -0500

    added output

--- script.js
+++ script.js
@@ -4,4 +4,40 @@ function getHistory() {
 function printHistory(num) {
     document.getElementById("history-value").innerText=num;
 }
-printHistory("9*9+8");
+function getOutput() {
+    return document.getElementById("output-value").innerText;
+}
+function printOutput(num) {
+    document.getElementById("output-value").innerText=getFormattedNumber(num);
+    if (num=="") {
+        document.getElementById("output-value").innerText=num;
+    }
+    else {
+        document.getElementById("output.value").innerText=getFormattedNumber(num);
+    }
+}
+function getFormattedNumber(num) {
+    var n =Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+function reverseNumberFormat(num) {
+    return Number(num.replace(/, /g, ''));
+}
+var operator = document.getElementsByClassName("operator");
+for (var i=0;i<operator.length;i++) {
+    operator[i].addEventListener("click", function() {
+        
+    });
+}
+var number = document.getElementsByClassName("number");
+for (var i=0;i<number.length;i++) {
+    number[i].addEventListener("click",function() {
+        var output=reverseNumberFormat(getOutput());
+        if(output!=NaN) {
+            output=output+this.id;
+            printOutput(output);
+    }
+});
+}
+
